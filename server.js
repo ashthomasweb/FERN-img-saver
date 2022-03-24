eee7e6ea9974239915fb9f97554cf339b7b253f3
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2022-03-24 15:26:43 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,107 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const itemRoutes = express.Router()
+let port = process.env.PORT || 4000
+let imgKey = process.env.UNSPLASH_API_KEY
+
+let Item = require('./src/models/item.model')
+const { default: axios } = require('axios')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+// change database variable to project appropriate name
+// find and replace acorss all files
+mongoose.connect('mongodb://127.0.0.1:27017/mernTemp', { useNewUrlParser: true })
+const connection = mongoose.connection
+
+connection.once('open', function () {
+  console.log('Local MongoDB database connection successfully established here')
+})
+
+function resAllWithMessage(message, res) {
+  Item.find({}, (error, items) => {
+    if (error) {
+      console.log(error)
+    } else {
+      res.json({message, items})
+    }
+  })
+}
+
+itemRoutes.route('/').get(function (req, res) {
+  console.log('home route')
+  resAllWithMessage('Home routed success!', res)
+})
+
+itemRoutes.route('/item/:id').get(function (req, res) {
+  let id = req.params.id
+  Item.findById(id, function (error, item) {
+    res.json({message: 'Item retrieved', item: item})
+  })
+})
+
+itemRoutes.route('/update/:id').post(function (req, res) {
+  Item.findById(req.params.id, function (error, item) {
+    if (!item) res.status(404).send('No data with that ID found')
+    else {
+      const { description, comment, rating, imageURL, photographer } = req.body
+      item.description = description
+      item.comment = comment
+      item.rating = rating
+      item.imageURL = imageURL
+      item.photographer = photographer
+    }
+    item.save()
+      .then(() => resAllWithMessage('Updated!', res))
+      .catch((error) => {
+        res.status(400).send('Update not possible')
+      })
+  })
+})
+
+itemRoutes.route('/add').post(function (req, res) {
+  const { description, comment, rating, imageURL, photographer } = req.body
+  let item = new Item()
+  item.description = description
+  item.comment = comment
+  item.rating = rating
+  item.imageURL = imageURL
+  item.photographer = photographer
+  item
+    .save()
+    .then(() => resAllWithMessage('Added!', res))
+    .catch((error) => {
+      res.status(400).send('Adding new item failed')
+    })
+})
+
+itemRoutes.route('/delete/:id').post(function (req, res) {
+  let _id = req.params.id
+  Item.deleteOne({ _id })
+    .then(() => resAllWithMessage('Deleted!', res))
+    .catch((error) => {
+      res.status(400).send('Deleting new item failed')
+    })
+})
+
+itemRoutes.route('/image/').get(function (req, res) {
+  let url = `https://api.unsplash.com/photos/random/?client_id=${imgKey}`
+  axios.get(url).then((response) => {
+    let data = response.data
+    let url = data.urls.regular
+    let name = data.user.name
+    res.json({message: 'image route reached', url, name })
+  })
+})
+
+app.use('/mernTemp', itemRoutes)
+
+// Listener compatible with Heroku, Localhost
+app.listen(port, () => console.log(`Server accessible at port ${port}.`))
+
+// END of document
---
+++ src/components/create-item/create-item.component.js
@@ -0,0 +1,155 @@
+import React, { useContext, useEffect } from 'react'
+import axios from 'axios'
+import { MainContext } from '../../context/main/MainState'
+
+function CreateItem() {
+  const { state: { tempItem }, dispatch } = useContext(MainContext)
+  let { imageURL, photographer } = tempItem
+
+  // controlled input elements
+  function changeDescription(e) {
+    tempItem.description = e.target.value
+    dispatch({ type: 'ONCHANGE_DESC', payload: tempItem })
+  }
+
+  function changeComment(e) {
+    tempItem.comment = e.target.value
+    dispatch({ type: 'ONCHANGE_COMMENT', payload: tempItem })
+  }
+
+  function changeRating(e) {
+    tempItem.rating = e.target.value
+    dispatch({ type: 'ONCHANGE_RATING', payload: tempItem })
+  }
+
+  useEffect(() => {
+    dispatch({ type: 'CLEAR_ITEM' })
+  }, [dispatch])
+
+  function imageRetrieval() {
+    axios.get('http://localhost:4000/mernTemp/image').then((response) => {
+      dispatch({ type: 'SET_RANDOM_IMAGE', payload: response.data })
+    })
+  }
+
+  // data handling
+  function onSubmit(e) {
+    e.preventDefault()
+    // send form data to server 'create' route
+    axios.post('http://localhost:4000/mernTemp/add', tempItem).then((response) => {
+      dispatch({ type: 'SET_ALL_ITEMS', payload: response.data })
+    })
+    dispatch({ type: 'CLEAR_ITEM' })
+  }
+
+  return (
+    <div style={{ margin: '10px auto', width: '95vw' }}>
+      <h3 style={{ textAlign: 'center' }}>Create New Item</h3>
+      { imageURL && (
+          <div style={{ textAlign: 'center', width: '40vw', margin: '0 auto', position: 'relative'}}>
+            <img src={imageURL} style={{width: '100%', position: 'relative'}} alt='no alt yet'/>
+            <p>Photo by {photographer}, courtesy of Unsplash API</p>
+          </div>
+        )
+      }
+
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label>Description: </label>
+          <input
+            type='text'
+            className='form-control'
+            value={tempItem.description}
+            onChange={changeDescription}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label>Comment: </label>
+          <input
+            type='text'
+            className='form-control'
+            value={tempItem.comment}
+            onChange={changeComment}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label>URL: </label>
+          <p
+            className='form-control'
+            style={{ overflowWrap: 'anywhere' }}
+            disabled
+          >{imageURL}</p>
+        </div>
+
+        <div className='form-group'>
+          <label>Photographer: </label>
+          <p
+            className='form-control'
+            style={{ overflowWrap: 'anywhere' }}
+            disabled
+          >{photographer}</p>
+        </div>
+
+        <div className='form-group'>
+          <div className='form-check form-check-inline'>
+            <label className='form-check-label'>
+              <input
+                className='form-check-input'
+                type='radio'
+                value='Low'
+                checked={tempItem.rating === 'Low'}
+                onChange={changeRating}
+              />
+              Low
+            </label>
+          </div>
+
+          <div className='form-check form-check-inline'>
+            <label className='form-check-label'>
+              <input
+                className='form-check-input'
+                type='radio'
+                value='Medium'
+                checked={tempItem.rating === 'Medium'}
+                onChange={changeRating}
+              />
+              Medium
+            </label>
+          </div>
+
+          <div className='form-check form-check-inline'>
+            <label className='form-check-label'>
+              <input
+                className='form-check-input'
+                type='radio'
+                value='High'
+                checked={tempItem.rating === 'High'}
+                onChange={changeRating}
+              />
+              High
+            </label>
+          </div>
+        </div>
+
+        <div className='form-group'>
+          <input
+            type='submit'
+            value='Create Item'
+            className='btn btn-primary'
+          />
+          <button
+            type='button'
+            className='btn btn-primary'
+            onClick={() => imageRetrieval()}
+          >Image</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default CreateItem
+
+// END of document
